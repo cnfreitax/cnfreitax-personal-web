a20891c9996541d6ad258433275c116c394e86aa
Victor Freitas <victorprogr@gmail.com>
2022-07-21 01:07:24 -0300

    refactor: add dynamic route to post page

--- src/components/PostList/index.tsx
+++ src/components/PostList/index.tsx
@@ -6,11 +6,19 @@ export const PostList = ({ posts }: PostListType) => {
   return (
     <S.Wrapper>
       {posts.map((post) => (
-        <Link href="/post" key={post.id}>
+        <Link
+          href={{
+            pathname: '/post/[id]',
+            query: {
+              id: post.id
+            }
+          }}
+          key={post.id}
+        >
           <S.PostBox>
             <div>
               <S.PostTile>{post.title}</S.PostTile>
-              <span className="span-see-more">ver mais</span>
+              <span className="span-see-more">clique para ver mais</span>
             </div>
             <S.PostPreView>{post.contentPreview}</S.PostPreView>
           </S.PostBox>
